a06da080b57fcb6d12cabc5660c72a887e2e537a
Anton Tkachenko <tkachenkoa2003@gmail.com>
2026-05-28 09:28:18 +0300

    mongoose fix

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -49,8 +49,8 @@ const userSchema = new mongoose.Schema({
   updatedAt: { type: Date, default: Date.now },
 })
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return
   this.password = await bcrypt.hash(this.password, 12)
   this.updatedAt = Date.now()
   next()
